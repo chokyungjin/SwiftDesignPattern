5046b2fa684e3e844981073bba2d90b4745fd498
Chokyungjin <46750574+chokyungjin@users.noreply.github.com>
2020-02-18 13:44:26 +0900

    create Practice Project

---
+++ CleanStorePractice/CleanStorePractice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CleanStorePractice
+//
+//  Created by 조경진 on 2020/02/18.
+//  Copyright © 2020 조경진. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+
